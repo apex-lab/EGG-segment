327515475bfdc460597e8c102f3df301625ef513
john-veillette <johnv@uchicago.edu>
2019-08-14 18:33:12 -0500

    made main script in segment.m

---
+++ segment.m
@@ -0,0 +1,22 @@
+% creates markers for the onset of voicing from an EGG time-series stored
+% in an xdf file output by labstreaminglayer
+
+%%% hard coded variables
+filename = "/home/john/Documents/EGG_pilot/sub-michelle/ses-S001/eeg/sub-michelle_ses-S001_task-egg_pilot_run-003_eeg.xdf";
+stream = 2; % which stream in the xdf file is EGG/audio
+channel = 1; % which channel of EGG/audio is EGG
+
+
+%%% extract EGG from xdf file
+xdf = load_xdf(filename); 
+egg = xdf{stream}.time_series(channel,:);
+
+%%% get markers from EGG
+windows = get_windows(egg); % candidate windows to search for voicing onset
+markers = get_markers(egg, windows); % exact timestamps of voicing onset
+
+
+%%% save as mat object that can be uploaded to EEGLab
+tags = to_eeglab(markers):
+saveName = strcat(char(filename)(1:end-3), "_tags.mat");
+save(savename, tags); % overwrites file "savename" if already there
