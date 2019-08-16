554501c8d401c312eedbc27b105ccc666caa028e
john-veillette <johnv@uchicago.edu>
2019-08-16 11:32:08 -0500

    Wrote function to find candidate windows for EGG analysis

---
+++ functions/get_windows.m
@@ -0,0 +1,35 @@
+function windows = get_windows(egg)
+% finds candidate windows where voicing has started to occur 
+
+    % get all peaks that exceed certain threshold (1 sd of sig)
+    sd = std(egg);
+    [~,locs_Rwave] = findpeaks(egg, 'MinPeakHeight', sd);
+
+    % remove all peaks but first of each cluster/sound
+    sz = size(locs_Rwave);
+    locs = 1:sz;
+    locs(1) = locs_Rwave(1);
+    for i = 2:sz(2)
+        if locs_Rwave(i) - locs_Rwave(i - 1) < 1000 % should be big enough
+            locs(i) = 0;                      % to encompass 2 consecutive
+        else                                  % peaks of any speaker's f0
+            locs(i) = locs_Rwave(i);
+        end
+    end
+    locs = locs(locs ~= 0);
+
+    % now we take intervals before each remaining peak and slide back in 
+    % time until the variance of the values in the window is near zero
+    var_length = 500;
+    start = locs - var_length; % start of each window
+    for i = 1:length(start)
+        while var((egg(start(i):start(i)+var_length))) > 1e-4
+            start(i) = start(i) - 1;
+        end
+    end
+    
+    % now we select our windows for further analysis as those 
+    % beginning at "start" and ending at the peaks remaining in "locs"
+    windows = transpose([start; locs]);
+
+end
--- segment.m
+++ segment.m
@@ -1,23 +1,26 @@
 % creates markers for the onset of voicing from an EGG time-series stored
 % in an xdf file output by labstreaminglayer
+% Tested with Matlab Version 9.6.0.1150989 (R2019a) Update 4
 
-%%% hard coded variables
-filename = "/home/john/Documents/EGG_pilot/sub-michelle/ses-S001/eeg/sub-michelle_ses-S001_task-egg_pilot_run-003_eeg.xdf";
+%% hard coded variables
+filepath = "/home/john/Documents/EGG_pilot/sub-marisa/ses-S001/eeg/sub-marisa_ses-S001_task-2_acq-eeg-egg_run-001_eeg.xdf";
 stream = 2; % which stream in the xdf file is EGG/audio
 channel = 1; % which channel of EGG/audio is EGG
 
-
-%%% extract EGG from xdf file
-xdf = load_xdf(filename); 
+addpath("functions", "peakdet2", "xdf-Matlab")
+    
+%% extract EGG from xdf file
+xdf = load_xdf(filepath); 
 egg = xdf{stream}.time_series(channel,:);
 
-%%% get markers from EGG
+%% get markers from EGG
 windows = get_windows(egg); % candidate windows to search for voicing onset
 markers = get_markers(egg, windows); % exact timestamps of voicing onset
 
-
-%%% save as mat object that can be uploaded to EEGLab
-tags = to_eeglab(markers):
-tmp = char(filename);
-saveName = strcat(tmp(1:end-3), "_tags.mat");
-save(savename, tags); % overwrites file "savename" if already there
+[q,r] = findchangepts(egg, 'Statistic', 'rms', 'MaxNumChanges');
+findchangepts(egg);%, 'Statistic', 'rms', 'MaxNumChanges')
+%% save as mat object that can be uploaded to EEGLab
+tags = to_eeglab(matlab);
+tmp = char(filepath);
+savepath = strcat(tmp(1:end-3), "_tags.mat");
+save(savepath, tags); % overwrites file "savepath" if already there
